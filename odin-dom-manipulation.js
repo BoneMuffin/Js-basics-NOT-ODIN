e6af0365206c994d9b7de4251746e65876f9c957
valen <valen.bagaglia@gmail.com>
2022-09-16 09:59:29 -0300

    added the basics

---
+++ odin-dom-manipulation.js
@@ -0,0 +1,10 @@
+// your JavaScript file
+const container = document.querySelector('#container');
+  
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const paragraph = document.createElement('p');
