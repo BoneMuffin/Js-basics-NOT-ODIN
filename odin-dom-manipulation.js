d5092af866b7a08ff054f3fabdfd752e48f6b0cb
valen <valen.bagaglia@gmail.com>
2022-09-16 10:57:18 -0300

    added the rest of the elements

--- odin-dom-manipulation.js
+++ odin-dom-manipulation.js
@@ -10,12 +10,26 @@ const paragraph = document.createElement('p');
     paragraph.style.color = 'red';
     paragraph.textContent = 'Red, I am';
 
+container.appendChild(paragraph);
+
 const h3 = document.createElement('h3');
 h3.style.color = 'blue';
 h3.textContent = 'Blue, I am';
 
-container.appendChild('h3');
+container.appendChild(h3);
 
 const div2 = document.createElement('div');
 div2.style.border = 'black';
 div2.style.background = 'pink';
+
+content.appendChild(div2);
+
+const h1 = document.createElement('h1');
+h1.textContent = "I'm a div";
+
+div2.appendChild(h1);
+
+const p = document.createElement('p');
+p.textContent = 'Me too';
+
+div2.appendChild(p);
