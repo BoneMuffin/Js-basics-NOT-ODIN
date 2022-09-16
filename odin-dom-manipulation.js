43416491bdd3c1aaf7a7432c92457f65f6598248
valen <valen.bagaglia@gmail.com>
2022-09-16 10:39:55 -0300

    created first two divs

--- odin-dom-manipulation.js
+++ odin-dom-manipulation.js
@@ -1,4 +1,3 @@
-// your JavaScript file
 const container = document.querySelector('#container');
   
 const content = document.createElement('div');
@@ -8,3 +7,15 @@ content.textContent = 'This is the glorious text-content!';
 container.appendChild(content);
 
 const paragraph = document.createElement('p');
+    paragraph.style.color = 'red';
+    paragraph.textContent = 'Red, I am';
+
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+h3.textContent = 'Blue, I am';
+
+container.appendChild('h3');
+
+const div2 = document.createElement('div');
+div2.style.border = 'black';
+div2.style.background = 'pink';
